fb96e02e6f43fcce68cfee2201a90440c2cdecc9
Larry <larry.aviles.la@gmail.com>
2025-11-17 23:59:41 -0600

    chore: add @astrojs/vercel dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   i18n: {
@@ -20,4 +22,5 @@ export default defineConfig({
   },
 
   integrations: [react()],
+  adapter: vercel(),
 });
