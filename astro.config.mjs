e2b2f7d6ab216c1ae1e7281a6d5a51137e8bba31
LarryAviles <larry.aviles.la@gmail.com>
2026-08-18 23:02:04 -0600

    fix: support Dokploy deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: true,
+  },
+
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
@@ -22,9 +26,7 @@ export default defineConfig({
   },
 
   integrations: [react()],
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
+  adapter: node({
+    mode: "standalone",
   }),
 });
